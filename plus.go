42a6fa0ca45432b93aa0a051226b265267d11649
Markus Wüstenberg <markus@maragu.dk>
2021-05-20 21:37:20 +0200

    Add lead paragraph on both pages

--- plus.go
+++ plus.go
@@ -15,7 +15,8 @@ func PlusPage() g.Node {
 
 		Div(
 			H1(g.Text("gomponents +")),
-			P(g.Raw(`gomponents really shines if we start building reusable components together. 🌟 Have you made a project with gomponents that other developers can use? <a href="https://github.com/maragudk/gomponents.com/issues/new">Create an issue on Github</a> and we can work together to add it here. 😎`)),
+			P(Class("lead"), g.Text(`gomponents really shines if we build reusable components together. 🌟`)),
+			P(g.Raw(`Have you made a project with gomponents that other developers can use? <a href="https://github.com/maragudk/gomponents.com/issues/new">Create an issue on Github</a> and we can work together to add it here. 😎`)),
 
 			H2(g.Text(`gomponents + TailwindCSS`)),
 			P(g.Raw(`gomponents works great together with <a href="https://tailwindcss.com">TailwindCSS</a>. In fact, this page is built using gomponents and TailwindCSS! Check out <a href="https://github.com/maragudk/gomponents.com">the source of this page</a> or <a href="https://github.com/maragudk/gomponents-tailwind-example">a gomponents + TailwindCSS example project</a>.`)),
