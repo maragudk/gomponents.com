3ca2e9b91a2fbd5dd03cc60e37c9e3b188a63ed6
Markus Wüstenberg <markus@maragu.dk>
2021-05-21 12:56:57 +0200

    Add gomponents + heroicons section on plus page

--- plus.go
+++ plus.go
@@ -20,6 +20,9 @@ func PlusPage() g.Node {
 
 			H2(g.Text(`gomponents + TailwindCSS`)),
 			P(g.Raw(`gomponents works great together with <a href="https://tailwindcss.com">TailwindCSS</a>. In fact, this page is built using gomponents and TailwindCSS! Check out <a href="https://github.com/maragudk/gomponents.com">the source of this page</a> or <a href="https://github.com/maragudk/gomponents-tailwind-example">a gomponents + TailwindCSS example project</a>.`)),
+
+			H2(g.Text(`gomponents + Heroicons`)),
+			P(g.Raw(`<a href="https://heroicons.com">Heroicons</a> are a collection of handcrafted SVG icons, by the makers of TailwindCSS. They work great together with gomponents! Check out <a href="https://github.com/maragudk/gomponents-heroicons">the gomponents-heroicons library on Github</a>.`)),
 		),
 	)
 }
