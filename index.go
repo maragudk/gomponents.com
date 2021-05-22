c2523320106580d4800b70daec325d4bc69b06fa
Markus Wüstenberg <markus@maragu.dk>
2021-05-22 12:38:00 +0200

    Adjust text on front page

--- index.go
+++ index.go
@@ -38,7 +38,7 @@ func IndexPage() g.Node {
 		Div(
 			H1(g.Text("Tired of complex template languages?")),
 			H2(g.Text("Try view components in pure Go.")),
-			P(Class("lead"), g.Raw(`<em>gomponents</em> are view components written in pure Go. They render to HTML 5, and make it easy for you to build reusable components. So you can focus on building your app instead of learning yet another templating language.`)),
+			P(Class("lead"), g.Raw(`<em>gomponents</em> are view components in pure Go. They render to HTML 5, and make it easy for you to build reusable components. So you can focus on building your app instead of learning yet another templating language.`)),
 			BashBlock("$ go get -u github.com/maragudk/gomponents"),
 			P(A(Href("https://github.com/maragudk/gomponents"), g.Text("See gomponents on Github"))),
 
