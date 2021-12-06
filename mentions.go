d6d66a7965571e078c89a19c1433dc7f2653e67e
Markus Wüstenberg <markus@maragu.dk>
2021-12-06 12:54:03 +0100

    Move lead from mentions page up

--- mentions.go
+++ mentions.go
@@ -13,9 +13,9 @@ func MentionsPage() g.Node {
 		"/mentions/",
 		Div(
 			H1(g.Text("Mentions")),
+			P(Class("lead"), g.Text("Shoutouts to gomponents around the internet.")),
 
 			H2(g.Text(`Twitter`)),
-			P(Class("lead"), g.Text("Shoutouts to gomponents around the internet.")),
 			g.Raw(`<blockquote class="twitter-tweet" data-dnt="true" data-theme="light"><p lang="en" dir="ltr">This is so cool. It&#39;s the most interesting GO library I&#39;ve seen lately. <a href="https://t.co/8bq841yvtq">https://t.co/8bq841yvtq</a></p>&mdash; mpj_marshall (@mpj_marshall) <a href="https://twitter.com/mpj_marshall/status/1392298708860014595?ref_src=twsrc%5Etfw">May 12, 2021</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`),
 
 			H2(g.Text(`Blogs`)),
